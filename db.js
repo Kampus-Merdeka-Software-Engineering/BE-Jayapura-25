c43d469af5dcd146a5e455a21fbc4275948a91d2
Steven Adi <stevenadi006@gmail.com>
2023-10-04 22:37:19 +0700

    ssl

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const conn = new sequelize.Sequelize("mysql://avnadmin:AVNS_hNqJ1VIUgBr37vPVMJ0@mysql-3dbd2932-jayapura25-capstoneproject.aivencloud.com:17670/DB_MHET?ssl-mode=REQUIRED", {
-  ssl: fs.readFileSync(path.join(__dirname, "credential.pem")),
+  ssl: fs.readFileSync("credential.pem"),
   dialect: "mysql",
   logging: false,
 });
